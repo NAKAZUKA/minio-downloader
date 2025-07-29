d079adf8168a546ff82c5ed4949000a6e44cd409
andrey <mishin23042002@gmai.com>
2025-07-29 11:42:30 +0300

    update minio_client.py

--- downloader/minio_client.py
+++ downloader/minio_client.py
@@ -22,28 +22,28 @@ class MinioDownloader:
         if not os.path.exists(Config.DOWNLOAD_DIR):
             os.makedirs(Config.DOWNLOAD_DIR)
 
-        def download_tif_files(self):
-            try:
-                logger.info("MINIO: Starting to list objects")
-                objects = self.client.list_objects(Config.MINIO_BUCKET, prefix=Config.MINIO_PREFIX, recursive=True)
-                for obj in objects:
-                    if not obj.object_name.lower().endswith(".tif"):
-                        continue
+    def download_tif_files(self):
+        try:
+            logger.info("MINIO: Starting to list objects")
+            objects = self.client.list_objects(Config.MINIO_BUCKET, prefix=Config.MINIO_PREFIX, recursive=True)
+            for obj in objects:
+                if not obj.object_name.lower().endswith(".tif"):
+                    continue
 
-                    if obj.last_modified is None:
-                        continue
+                if obj.last_modified is None:
+                    continue
 
-                    obj_time = obj.last_modified.astimezone(Config.MOSCOW_TZ)
+                obj_time = obj.last_modified.astimezone(Config.MOSCOW_TZ)
 
-                    if not (Config.START_DATE <= obj_time <= Config.END_DATE):
-                        continue
+                if not (Config.START_DATE <= obj_time <= Config.END_DATE):
+                    continue
 
-                    file_path = os.path.join(Config.DOWNLOAD_DIR, os.path.basename(obj.object_name))
-                    logger.info(f"MINIO: Downloading {obj.object_name} to {file_path}")
-                    self.client.fget_object(Config.MINIO_BUCKET, obj.object_name, file_path)
-            except S3Error as e:
-                logger.error(f"MINIO: Error accessing object - {e}")
-                raise
-            except Exception as e:
-                logger.error(f"MINIO: Unknown error during download - {e}")
-                raise
+                file_path = os.path.join(Config.DOWNLOAD_DIR, os.path.basename(obj.object_name))
+                logger.info(f"MINIO: Downloading {obj.object_name} to {file_path}")
+                self.client.fget_object(Config.MINIO_BUCKET, obj.object_name, file_path)
+        except S3Error as e:
+            logger.error(f"MINIO: Error accessing object - {e}")
+            raise
+        except Exception as e:
+            logger.error(f"MINIO: Unknown error during download - {e}")
+            raise
